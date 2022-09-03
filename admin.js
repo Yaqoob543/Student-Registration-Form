470332f79250f32b4b5cfe1e6bb4168ff003493b
Yaqoob <sualehfarooq65@gmail.com>
2022-09-03 14:06:01 -0700

    Commit

---
+++ admin.js
@@ -0,0 +1,18 @@
+var table = document.getElementById('table')
+
+
+
+
+var create = `  <tr>
+<td>1</td>
+<td>Sualeh</td>
+<td>Father</td>
+<td>Phone</td>
+<td>Course</td>
+<td>City</td>
+<td>Cnic</td>
+<td>Picture</td>
+
+</tr>`
+
+table.innerHTML += create
--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 document.getElementById('card').style.display = 'none'
+    // document.getElementById('form').style.display = 'none'
+
 
 function idCard() {
     document.getElementById('form').style.display = 'none'
     document.getElementById('card').style.display = 'block'
-    show()
-
 }
 
 function disform() {
@@ -13,7 +13,7 @@ function disform() {
 
 }
 
-var nameReg = /^[a-zA-Z]+[a-zA-Z]+$/;
+var nameReg = /^[a-zA-Z] +[a-zA-Z]+$/;
 var emailReg = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
 var numReg = /^[\+]?[(]?[0-9]{4}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,5}$/
 var addReg = /^[0-9a-zA-Z\s,-]+$/
@@ -44,7 +44,12 @@ function getData() {
                                         if (qualification.value) {
                                             if (pic.value) {
                                                 // Further Working Start from here when all fields meet the requirement
-                                                alert('All Set')
+                                                Swal.fire(
+                                                    'Registered Successfully!',
+                                                    fullName.value + ' You can Download Your ID Card ',
+                                                    'success'
+                                                )
+                                                fetchData()
 
                                             } else {
                                                 swal("Picture Missing", "Upload Your Photo", "info");
@@ -83,11 +88,68 @@ function getData() {
 }
 
 
+function Student(pic, name, father, number, cnic, course, city, ) {
+    this.pic = pic
+    this.name = name
+    this.father = father
+    this.number = number
+    this.cnic = cnic
+    this.course = course
+    this.city = city
+}
+
+
+var count = 1
+
+function fetchData() {
+    b[`ID# ${count}`] = new Student(pic.value, fullName.value, fatherName.value, number.value, cnic.value, course.value, city.value)
+    localStorage.setItem('data', JSON.stringify(b))
+    console.log(b)
+    count++
+}
+
+var a = JSON.parse(localStorage.getItem('data'))
+
+if (a) {
+    var b = a
+} else {
+    var b = {}
+}
+console.log(Object.keys(a).length)
+
 function checkCard() {
     var cnic = document.getElementById('cnic2')
     if (cnicReg.test(cnic.value)) {
-        alert('Ok')
+        showCard()
     } else {
         swal("Invalid CNIC", "Please Enter CNIC Correct Format ", "info");
+    }
+}
+
+function cardReturn() {
+    document.getElementById('cardSpace').style.display = 'none'
+}
+
+
+function showCard() {
+    var cnic = document.getElementById('cnic2')
+    var check = JSON.parse(localStorage.getItem('data'))
+    for (var keys in check) {
+        if (check[keys].cnic == cnic.value) {
+            var pic = check[keys].pic
+            var firstName = check[keys].name
+            var fatherName = check[keys].father
+            var course = check[keys].course
+            document.getElementById('cardSpace').style.display = 'block'
+            var img = document.getElementById('dp')
+            img.src = pic
+            var name = document.getElementById('cardName')
+            name.innerText = firstName
+            var father = document.getElementById('cardFather')
+            father.innerText = fatherName
+            var coursecheck = document.getElementById('cardCourse')
+            coursecheck.innerText = course
+            break
+        }
     }
 }
